654fa8997a01b4ca21e16aa51b0bc75de34affac
ShaikAbdulRazzaaq <43449008+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-03-06 02:58:23 +0530

    Adding The ViewPager

--- app/src/main/java/com/razzaaq/fragmentsexample/ViewPagerFragment.java
+++ app/src/main/java/com/razzaaq/fragmentsexample/ViewPagerFragment.java
@@ -8,6 +8,9 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import java.util.Objects;
 
@@ -20,6 +23,27 @@ public class ViewPagerFragment extends Fragment {
         int position=getArguments().getInt(KEY_RECIPE_INDEX);
         Objects.requireNonNull(getActivity()).setTitle(recipes.names[position]);
         View view= LayoutInflater.from(getContext()).inflate(R.layout.fragment_view_pager,container,false);
+        ViewPager viewPager=view.findViewById(R.id.viewPager);
+        final IngredientsFragment ingredientsFragment=new IngredientsFragment();
+        final DirectionsFragments directionsFragments=new DirectionsFragments();
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+               return position==0 ? ingredientsFragment:directionsFragments;
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
         return view;
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Objects.requireNonNull(getActivity()).setTitle(getResources().getString(R.string.app_name));
     }
 }
