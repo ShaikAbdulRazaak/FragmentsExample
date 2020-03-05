f31a265e6a14832810589acb1febf6ed2795e823
ShaikAbdulRazzaaq <43449008+ShaikAbdulRazzaaq@users.noreply.github.com>
2020-03-06 04:08:32 +0530

    Worked on IngredientFragment

--- app/src/main/java/com/razzaaq/fragmentsexample/IngredientsFragment.java
+++ app/src/main/java/com/razzaaq/fragmentsexample/IngredientsFragment.java
@@ -4,16 +4,58 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class IngredientsFragment extends Fragment {
+    private static final String KEY_BOOLEAN_CHECKBOXES ="key_boolean_checkboxes" ;
+    private CheckBox[] checkBoxes;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=LayoutInflater.from(getContext()).inflate(R.layout.fragment_ingredients,container,false);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_ingredients, container, false);
+        assert getArguments() != null;
+        int position = getArguments().getInt(ViewPagerFragment.KEY_RECIPE_INDEX);
+        LinearLayout linearLayout = view.findViewById(R.id.ingredients_Layout);
+        String[] Ingredients = recipes.ingredients[position].split("`");
+        checkBoxes = new CheckBox[recipes.ingredients.length];
+        boolean[] checkedBoxes= new boolean[checkBoxes.length];
+        if(savedInstanceState!=null && savedInstanceState.getBooleanArray(KEY_BOOLEAN_CHECKBOXES)!=null){
+            checkedBoxes=savedInstanceState.getBooleanArray(KEY_BOOLEAN_CHECKBOXES);
+        }
+        setUpCheckBoxes(Ingredients, linearLayout,checkedBoxes);
         return view;
+    }
+
+    private void setUpCheckBoxes(String[] ingredients, ViewGroup container, boolean[] checkedBoxes) {
+        int i = 0;
+        for (String ingredient : ingredients) {
+            checkBoxes[i] = new CheckBox(getContext());
+            checkBoxes[i].setPadding(8, 16, 8, 16);
+            checkBoxes[i].setTextSize(20f);
+            checkBoxes[i].setText(ingredient);
+            container.addView(checkBoxes[i]);
+            i++;
+            if(checkedBoxes[i]){
+                checkBoxes[i].toggle();
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        boolean[] saveStateOfCheckBoxes = new boolean[checkBoxes.length];
+        int i=0;
+        for(CheckBox checkBox:checkBoxes){
+            saveStateOfCheckBoxes[i]=checkBox.isChecked();
+            i++;
+        }
+        outState.putBooleanArray(KEY_BOOLEAN_CHECKBOXES,saveStateOfCheckBoxes);
+        super.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/razzaaq/fragmentsexample/ViewPagerFragment.java
+++ app/src/main/java/com/razzaaq/fragmentsexample/ViewPagerFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +12,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.Objects;
 
@@ -25,6 +29,9 @@ public class ViewPagerFragment extends Fragment {
         View view= LayoutInflater.from(getContext()).inflate(R.layout.fragment_view_pager,container,false);
         ViewPager viewPager=view.findViewById(R.id.viewPager);
         final IngredientsFragment ingredientsFragment=new IngredientsFragment();
+        Bundle bundle=new Bundle();
+        bundle.putInt(KEY_RECIPE_INDEX,position);
+        ingredientsFragment.setArguments(bundle);
         final DirectionsFragments directionsFragments=new DirectionsFragments();
         viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
             @NonNull
@@ -33,12 +40,19 @@ public class ViewPagerFragment extends Fragment {
                return position==0 ? ingredientsFragment:directionsFragments;
             }
 
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return position==0?"Ingredients":"Directions";
+            }
+
             @Override
             public int getCount() {
                 return 2;
             }
         });
-
+        TabLayout tabLayout=view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
         return view;
     }
     @Override
